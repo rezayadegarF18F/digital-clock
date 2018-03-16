2c11b12660e43f9160310045df54091fecac91f7
reza yadegar <rezayadegarF18F@gmail.com>
2018-03-16 16:34:16 +0330

    upload digital clock with relativlayout

---
+++ app/src/main/java/com/digitalclock_reza_yadegar_2018315/rezaa/digitalclock/MainActivity.java
@@ -0,0 +1,13 @@
+package com.digitalclock_reza_yadegar_2018315.rezaa.digitalclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
